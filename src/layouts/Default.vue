6f8b720a5c77d12941be2f2cf272cdc731d93ff9
Zachary Svoboda <svobodaz@orvis.com>
2019-11-13 18:51:59 -0500

    font-display setup

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -11,6 +11,7 @@
   font-family: 'Amatic SC';
   font-style: normal;
   font-weight: 400;
+  font-display: swap;
   src: local('Amatic SC Regular'), local('AmaticSC-Regular'),
        url('../assets/fonts/amatic-sc-v13-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
        url('../assets/fonts/amatic-sc-v13-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
@@ -21,6 +22,7 @@
   font-family: 'Noto Sans';
   font-style: normal;
   font-weight: 400;
+  font-display: swap;
   src: local('Noto Sans'), local('NotoSans'),
        url('../assets/fonts/noto-sans-v9-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
        url('../assets/fonts/noto-sans-v9-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
